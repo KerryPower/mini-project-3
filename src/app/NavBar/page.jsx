273547c70871a4be3a6d9938fa16d69b8b4cd854
KerryPower <kerrycpower@gmail.com>
2024-08-05 10:57:00 +1200

    bootstrap not working

---
+++ src/app/NavBar/page.jsx
@@ -0,0 +1,37 @@
+'use client'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavbarBrand, NavbarToggle, NavbarCollapse} from 'react-bootstrap';
+import NavLink from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <NavbarBrand href="#home">React-Bootstrap</NavbarBrand>
+        <NavbarToggle aria-controls="basic-navbar-nav" />
+        <NavbarCollapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink href="#home">Home</NavLink>
+            <NavLink href="#link">Link</NavLink>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </NavbarCollapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar
